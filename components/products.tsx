3e41b1c79f3f66abfcecc7d0af4289de9f2c4232
otnielssilva <otniel.dss04@gmail.com>
2024-03-27 12:50:57 -0400

    feat: adiciona componente 'products'

---
+++ components/products.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Product } from "./product";
+import { IProduct } from "../types/types";
+import styled from "styled-components";
+import { fetchProducts } from "../app/data";
+
+const StyledProducts = styled.div`
+	width: 80%;
+	margin: 0 auto;
+`;
+
+const StyledList = styled.ul`
+	list-style: none;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+`;
+
+export const Products = () => {
+	const [products, setProducts] = useState<IProduct[]>([]);
+
+	useEffect(() => {
+		fetchProducts().then((data) => setProducts(data));
+	}, []);
+
+	return (
+		<StyledProducts>
+			<StyledList>
+				{products.map((product) => (
+					<Product key={product.id} product={product} />
+				))}
+			</StyledList>
+		</StyledProducts>
+	);
+};
