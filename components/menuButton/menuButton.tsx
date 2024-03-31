1809aa11a427f4275cd77742fab6ba1f689474be
otnielssilva <otniel.dss04@gmail.com>
2024-03-31 01:12:10 -0400

    refactor: remove styled-components do package.json

--- components/menuButton/menuButton.tsx
+++ components/menuButton/menuButton.tsx
@@ -1,21 +1,11 @@
 'use client';
 import Link from 'next/link';
-import styled from 'styled-components';
 import './menuButton.css';
 
 interface Props {
   href: string;
   title: string;
 }
-
-const StyledButton = styled.button`
-	background-color: #555;
-	color: white;
-	border: none;
-	padding: 10px 20px;
-	cursor: pointer;
-	&:hover {
-		background-color: #777;`;
 
 export default function MenuButton({ href, title }: Props) {
   return (
