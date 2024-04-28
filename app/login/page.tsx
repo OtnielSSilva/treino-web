6753d06c876f3b961a3df96a5a331119e4c4388d
OtnielSSilva <otniel.dss04@gmail.com>
2024-04-28 13:08:49 -0400

    feat: implementa validações nos formulários

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,24 +3,44 @@ import React from 'react';
 import styles from './login.module.css';
 
 const LoginPage = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Authentication logic goes here
-  };
-
   return (
     <div className={styles.loginContainer}>
-      <form className={styles.loginForm} onSubmit={handleSubmit}>
+      <form
+        className={styles.loginForm}
+        // onSubmit={handleSubmit}
+        action='http://localhost:8180/login/'
+        method='POST'
+      >
         <h2 className={styles.formTitle}>Login</h2>
         <div className={styles.formInputBox}>
-          <input className={styles.formInput} type='email' required id='email' />
-          <label className={styles.formLabel} htmlFor='email'>Email</label>
+          <input
+            className={styles.formInput}
+            type='email'
+            required
+            id='email'
+            name='email'
+            pattern='[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,50}$'
+          />
+          <label className={styles.formLabel} htmlFor='email'>
+            Email
+          </label>
         </div>
         <div className={styles.formInputBox}>
-          <input className={styles.formInput} type='password' required id='password' />
-          <label className={styles.formLabel} htmlFor='password'>Senha</label>
+          <input
+            className={styles.formInput}
+            type='password'
+            required
+            id='senha'
+            name='senha'
+            pattern='.{8,}'
+          />
+          <label className={styles.formLabel} htmlFor='senha'>
+            Senha
+          </label>
         </div>
-        <button className={styles.submitButton} type='submit'>Entrar</button>
+        <button className={styles.submitButton} type='submit'>
+          Entrar
+        </button>
         <div className={styles.registerLink}>
           Não tem uma conta? <a href='#'>Registre-se</a>
         </div>
